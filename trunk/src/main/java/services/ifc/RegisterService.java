d5290e2c0dfc5661c2477fc5e0076047e915b010
ivald79 <ivald79@2a58e4c8-ede0-3a1d-5610-f8f8fdca9c7d>
2015-01-23 21:45:36 +0000

    Jersey rest + moxy + JAXB + WebServer - Fixed

--- trunk/src/main/java/services/ifc/RegisterService.java
+++ trunk/src/main/java/services/ifc/RegisterService.java
@@ -1,15 +1,12 @@
 package services.ifc;
 
-import models.UserEntity;
 import models.WebResponse;
-import repository.ifc.BaseIfc;
-
-import javax.ws.rs.PathParam;
+import models.ws.UserWSNEntity;
 
 /**
  * Created by ivald79 on 24/09/2014.
  */
 public interface RegisterService {
 
-    public WebResponse registration(String username,String password,String firstname,String lastname) throws Exception;
+    public WebResponse registration(UserWSNEntity userEntity) throws Exception;
 }
